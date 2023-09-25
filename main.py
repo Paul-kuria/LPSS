9987416eb3629aa6990e42c53f110d4c85ce0ae1
murani_k <paulmurani@gmail.com>
2023-09-25 19:44:04 +0300

    feat: added requirements.txt

--- main.py
+++ main.py
@@ -1,17 +1,30 @@
 import os 
-from logic import VideoInfer
+from cann_edge import VideoInfer
+from anpr_predict import YoloModel
 from api_request import get_one_record
+from pprint import pprint 
+
+load_weights = "./assets/anpr_v2.pt"
+load_images = "./datasets/plates"
 
 def main():
-    vehicles = "./datasets"
-    ANPR = VideoInfer()
-
-    for root, dirs, files in os.walk(vehicles):
+    
+    count = 1
+    anpr_plate_list = []
+    for root, dirs, files in os.walk(load_images):
         for file in sorted(files):
-            path = os.path.join(root, file)
-            print(file)
-            ANPR.image_manipulate(path)
-    # get_one_record(vehicle_plate)
+            imgpath = os.path.join(root, file)
+            # print(imgpath)
+            run = YoloModel(
+                img_source=imgpath,
+                weights_path=load_weights
+            )
+            vehicle_plate = run.predict(count)
+            result = get_one_record(vehicle_plate)
+            print(result)
+            # anpr_plate_list.append(dct)
+            count += 1
+    pprint(anpr_plate_list)
 
 if __name__ == "__main__":
     main()
---
+++ manual_methods/upload_many.py
@@ -0,0 +1,8 @@
+from ..api_request import upload_records
+
+
+file_source = "./member_registry.csv"
+
+def upload():
+    with open(file, 'r') as fdoc:
+        
